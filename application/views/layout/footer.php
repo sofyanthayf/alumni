0c2193d8afedd0fa9c9ec8119a4bd04301ba0a60
xasdqwex <rickiman10zx@gmail.com>
2019-01-30 20:43:50 +0800

    commit View

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -31,12 +31,25 @@
 <script src="/assets/lib/owlcarousel/owl.carousel.min.js"></script>
 <script src="/assets/lib/magnific-popup/magnific-popup.min.js"></script>
 <script src="/assets/lib/sticky/sticky.js"></script>
-
+<script src="/assets/js/bootstrap-datepicker.js"></script>
+<script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap4.min.js"></script>
 <!-- Contact Form JavaScript File -->
 <script src="contactform/contactform.js"></script>
 
 <!-- Template Main Javascript File -->
 <script src="/assets/js/main.js"></script>
+
+<script type="text/javascript">
+ $(function(){
+  $(".datepicker").datepicker({
+      format: 'yyyy-mm-dd',
+      autoclose: true,
+      todayHighlight: true,
+  });
+ });
+ $('#tabel_loker').DataTable();
+</script>
 
 </body>
 </html>
