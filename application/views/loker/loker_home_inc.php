36192424503c9ca366850c92daaed585de6e4a82
xasdqwex <rickiman10zx@gmail.com>
2019-01-30 20:50:10 +0800

    datetimepicker.js

--- application/views/loker/loker_home_inc.php
+++ application/views/loker/loker_home_inc.php
@@ -8,17 +8,37 @@
 
     <div class="row">
 
-      <?php foreach($loker as $l) { ?>
-
       <div class="col-lg-6">
         <div class="box wow fadeInLeft">
           <div class="icon"><i class="fa fa-bar-chart"></i></div>
-          <h4 class="title"><a href="apply_loker/<?= $l['id'] ?>"><?= $l['nama_perusahaan'] ?></a></h4>
-          <p class="description"><?= $l['deskripsi'] ?></p>
+          <h4 class="title"><a href="">Lorem Ipsum</a></h4>
+          <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident etiro rabeta lingo.</p>
         </div>
       </div>
 
-      <?php } ?>
+      <div class="col-lg-6">
+        <div class="box wow fadeInRight">
+          <div class="icon"><i class="fa fa-picture-o"></i></div>
+          <h4 class="title"><a href="">Dolor Sitema</a></h4>
+          <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata nodera clas.</p>
+        </div>
+      </div>
+
+      <div class="col-lg-6">
+        <div class="box wow fadeInLeft" data-wow-delay="0.2s">
+          <div class="icon"><i class="fa fa-shopping-bag"></i></div>
+          <h4 class="title"><a href="">Sed ut perspiciatis</a></h4>
+          <p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur trinige zareta lobur trade.</p>
+        </div>
+      </div>
+
+      <div class="col-lg-6">
+        <div class="box wow fadeInRight" data-wow-delay="0.2s">
+          <div class="icon"><i class="fa fa-map"></i></div>
+          <h4 class="title"><a href="">Magni Dolores</a></h4>
+          <p class="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum rideta zanox satirente madera</p>
+        </div>
+      </div>
 
     </div>
 
