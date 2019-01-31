40f0a40ef2c3daeec5e62f056dc0b350dc710b69
Ronaldo A. Lumintang <ronaldolumintang55@gmail.com>
2019-01-31 09:55:04 +0800

    Loker
    Loker Controller,Model,List Loker,Loker Home Inc

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -6,9 +6,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  */
 class Home extends CI_Controller {
 
+	public function __construct()
+	{
+		parent::__construct();
+
+		// libatkan file class Alumni_model.php , selanjutnya akan disebut sebagai '$this->alumni_model'
+		$this->load->model('alumni_model');
+		$this->load->model('loker_model');
+
+	}
+
 	public function index()
 	{
-		$this->load->template('homepage');
+		// ambil data dari Alumni_model melalui function jumlahAlumni()
+		$data['jumlah_alumni'] = $this->alumni_model->jumlahAlumni();
+		$data['loker'] = $this->loker_model->list_loker_home();
+		// passing data ke view homepage
+		$this->load->template('homepage', $data);
 	}
 
 
--- application/models/Loker_model.php
+++ application/models/Loker_model.php
@@ -6,6 +6,42 @@ class Loker_model extends CI_Model {
         parent::__construct();
     }
 
+    public function tambahLoker($params) {
+        $this->db->insert('lowongan_kerja',$params);
+    }
 
+    function get_loker($id)
+    {
+      $this->db->where('lowongan_kerja.id', $id);
+      $this->db->join('mitra','mitra.id_mitra = lowongan_kerja.mitra');
+      return $this->db->get('lowongan_kerja')->row_array();
+    }
+
+    function updateLoker($params)
+    {
+      $this->db->where('id', $params['id']);
+      return $this->db->update('lowongan_kerja',$params);
+    }
+
+    function get_nama_perusahaan($id_mitra)
+    {
+      $this->db->where('id_mitra',$id_mitra);
+      return $this->db->get('mitra')->row_array();
+    }
+
+    function list_loker_home()
+    {
+      $this->db->order_by('lowongan_kerja.id','DESC');
+      $this->db->limit(4);
+      $this->db->join('mitra','mitra.id_mitra = lowongan_kerja.mitra');
+      return $this->db->get('lowongan_kerja')->result_array();
+    }
+
+    function list_loker()
+    {
+      $this->db->order_by('lowongan_kerja.id','DESC');
+      $this->db->join('mitra','mitra.id_mitra = lowongan_kerja.mitra');
+      return $this->db->get('lowongan_kerja')->result_array();
+    }
 
 }
