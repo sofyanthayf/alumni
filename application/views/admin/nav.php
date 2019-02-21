47d11018c4a9ccb13d5a137d9d2d9d8007bc0cc7
Sofyan Thayf <sofyanthayf@gmail.com>
2019-02-21 11:34:56 +0800

    validasi alumni improved

--- application/views/admin/nav.php
+++ application/views/admin/nav.php
@@ -1,7 +1,9 @@
 
 <a class="btn btn-light btn-block" href="/admin/validasialumni" title="Ganti Password" style="margin:15px;">
-  <img src="/assets/img/mhs16.png"> Validasi Alumni (<?=count($validasiwaitinglist)>0?count($validasiwaitinglist):''?>)
+  <img src="/assets/img/mhs16.png">
+  Validasi Alumni <?=count($validasiwaitinglist)>0?'('.count($validasiwaitinglist).')':''?>
 </a>
+
 <a class="btn btn-light btn-block" href="/gantipassword" title="Ganti Password" style="margin:15px;">
   <img src="/assets/img/key16.png"> Ganti Password
 </a>
