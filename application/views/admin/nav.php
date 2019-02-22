b8af18fdc8bf9babe8550eecaad84a159c8c25cf
Sofyan Thayf <sofyanthayf@gmail.com>
2019-02-22 13:22:05 +0800

    admin improved

--- application/views/admin/nav.php
+++ application/views/admin/nav.php
@@ -7,11 +7,8 @@
 <?php else: ?>
 
   <a class="btn btn-light btn-block" href="/admin/validasialumni" title="Ganti Password" style="margin:15px;">
-    <img src="/assets/img/mhs16.png">
-    Validasi Alumni
-    <span class="badge badge-primary">
-      <?=count($validasiwaitinglist)>0 ? count($validasiwaitinglist) : ''?>
-    </span>
+    <img src="/assets/img/mhs16.png"> Validasi Alumni
+    <span class="badge badge-primary"><?=count($validasiwaitinglist)>0 ? count($validasiwaitinglist) : ''?></span>
   </a>
 
   <a class="btn btn-light btn-block" href="/gantipassword" title="Ganti Password" style="margin:15px;">
