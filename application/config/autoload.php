3eacaca337ac5f5c82b18e46da238c678a1f4595
sofyanthayf <sofyanthayf@gmail.com>
2021-10-21 09:24:01 +0800

    check email problem

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database','gcharts', 'session', 'pdf', 'email',
+$autoload['libraries'] = array('database','gcharts', 'session', 'pdf', 'ci_email',
                                'mylib', 'unirest', 'curl');
 
 /*
